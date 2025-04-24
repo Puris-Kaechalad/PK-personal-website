bfa53a1c67e4729591522c702faf781cf17e1faa
Puris Kaechalad <puris.naja.2004@gmail.com>
2025-04-25 04:11:38 +0700

    first commit

---
+++ src/component/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function nav() {
+    return (
+        <>
+            <div>
+                <div>
+                    <div>
+                        <img src="#" alt="logo" />
+                    </div>
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default nav
