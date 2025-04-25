c10b99dd390f89901385d60369bd82f51add11b7
Puris Kaechalad <puris.naja.2004@gmail.com>
2025-04-25 19:21:26 +0700

    set default

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/home.jsx'
 
 function App() {
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </Router>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Navbar from '../component/nav.jsx';
 
 function Home() {
     return (
         <>
-        
-        
-        
+            <Navbar />
+            <div className="flex justify-center items-center h-screen">
+                <h1 className="text-white text-6xl font-bold">
+                    hello world!!
+                </h1>
+            </div>
         </>
     )
 }
