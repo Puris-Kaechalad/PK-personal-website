d86aa753a2fba05dd1236d6ccbd7abd1b8854a3d
Puris Kaechalad <puris.naja.2004@gmail.com>
2025-05-01 16:56:00 +0700

    new navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <>
             <Navbar />
-            <div className="flex justify-center items-center h-screen">
+            <div className="flex justify-center items-center h-screen bg-sky-800">
                 <h1 className="text-white text-6xl font-bold">
                     hello world!!
                 </h1>
